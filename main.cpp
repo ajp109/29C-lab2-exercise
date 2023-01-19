84e02c1708581843af280f21171e524f5e78e3d2
ajp109 <andrew.pomfret@york.ac.uk>
2023-01-19 16:54:18 +0000

    Add project-specific files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include "mbed.h"
 
 int main() {
+
+    // Example list of numbers to sum:
+    int list[] = {1,-7,4,6,10,11,3,0,-1};
+
+    // Expected result is 27 with the provided numbers
+    printf("The sum of the numbers in the array is: %d\n", arraySum(list, 9));
+
+    // Uncomment this when you're ready to write the second function
+    // printIntArray(list, 9);
+
     while (true) {
         sleep();
     }
